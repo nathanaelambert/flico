5dc24d1226f60299124ee567c6ba7b66dc06f0e0
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-02-23 21:05:06 +0100

    fix basic test

--- API/basic_test.py
+++ API/basic_test.py
@@ -12,3 +12,9 @@ if not API_KEY or not API_SECRET:
     raise ValueError("FLICKR_API_KEY and FLICKR_API_SECRET must be set in .env")
 
 flickr_api.set_keys(api_key=API_KEY, api_secret=API_SECRET)
+
+photos = flickr_api.Photo.search(tags="sunset", per_page=10)
+
+for photo in photos:
+    print(photo.title)
+
