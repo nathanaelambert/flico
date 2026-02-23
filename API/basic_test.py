664cb652eaae5cf5297ce20e2df6634c60df3f12
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-02-23 20:52:58 +0100

    basic test API

---
+++ API/basic_test.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+import flickr_api
+
+# Load .env variables
+load_dotenv()
+
+API_KEY = os.getenv('FLICKR_API_KEY')
+API_SECRET = os.getenv('FLICKR_API_SECRET')
+
+if not API_KEY or not API_SECRET:
+    raise ValueError("FLICKR_API_KEY and FLICKR_API_SECRET must be set in .env")
+
+flickr_api.set_keys(api_key=API_KEY, api_secret=API_SECRET)
