636b3d7109112bd8fb460a36f3e591abb6dd2e6e
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-03-13 21:57:45 +0100

    add roles

---
+++ src/postgresql/002_roles_setup.sql
@@ -0,0 +1,19 @@
+\c "flickr_commons_metadata";
+
+CREATE ROLE crawler LOGIN ENCRYPTED PASSWORD 'xxx';
+CREATE ROLE dev     LOGIN ENCRYPTED PASSWORD 'yyy';
+CREATE ROLE server  LOGIN ENCRYPTED PASSWORD 'zzz';
+
+GRANT CONNECT ON DATABASE "flickr_commons_metadata" TO crawler, server, dev;
+GRANT USAGE ON SCHEMA public TO crawler, server, dev;
+GRANT SELECT, INSERT, UPDATE, DELETE ON institution TO crawler;
+GRANT SELECT, INSERT, UPDATE, DELETE ON photo       TO crawler;
+GRANT SELECT, INSERT, UPDATE, DELETE ON license       TO crawler;
+GRANT SELECT ON institution TO server;
+GRANT SELECT ON photo       TO server;
+GRANT SELECT ON license     TO server;
+GRANT ALL PRIVILEGES ON DATABASE "flickr_commons_metadata" TO dev;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO dev;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO dev;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES    TO dev;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO dev;
