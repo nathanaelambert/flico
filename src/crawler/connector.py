2d5325041b03b64f1efd9b03ce73726f44851e9f
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-03-16 18:13:18 +0100

    db connector crawler

--- src/crawler/connector.py
+++ src/crawler/connector.py
@@ -10,3 +10,14 @@ def get_flickr_endpoint():
     if not API_KEY or not API_SECRET:
         raise ValueError("FLICKR_API_KEY and FLICKR_API_SECRET must be set in .env")
     return flickrapi.FlickrAPI(API_KEY, API_SECRET, format='parsed-json')
+
+def get_db_connection():
+""" responsibility of closing the connection is on caller"""
+    load_dotenv()
+    return = psycopg2.connect(
+        dbname=os.getenv('PGDATABASE'), 
+        user="crawler", 
+        password=os.getenv('PWDCRAWLER'),
+        host=os.getenv('PGHOST'), 
+        port=os.getenv('PGPORT', '5432')
+    )
