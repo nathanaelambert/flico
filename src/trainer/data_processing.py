1b0f4d8d70eecf5baddc7a65bffd07981a92aa65
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-03-19 14:32:09 +0100

    trainer initial processing

---
+++ src/trainer/data_processing.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+from connector import get_db_connection
+
+def get_dated_photos():
+    """retrieves photos with dates from database"""
+    conn = get_db_connection()
+    query = """
+    SELECT date_taken, date_taken_granularity, owner_nsid, id
+    FROM photo 
+    WHERE date_taken IS NOT NULL
+    """
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    print(f"Loaded {len(df)} photos)
+    return df
+
+def filter_dated_photos(df):
+    """ only keeps photos with reasonable dates"""
+    df['year'] = pd.to_datetime(df['date_taken'], errors='coerce').dt.year
+    df = df.dropna(subset=['year'])
+    df = df[(df['year'] >= 1850) & (df['year'] <= 2026)]
+    df = df[df['owner_nsid'] != '12403504@N02'] # filtering the british library because all their dates are 2013 (unreliable dates)
+    print(f"Kept {len(df)} photos spanning {int(df['year'].min())}–{int(df['year'].max())}")
+    return df
+
