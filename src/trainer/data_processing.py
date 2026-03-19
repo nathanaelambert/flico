dfe2c098480b7b36c173d4bfbff0c8f4c56402ed
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-03-19 17:22:03 +0100

    db connector

--- src/trainer/data_processing.py
+++ src/trainer/data_processing.py
@@ -2,19 +2,18 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
-from connector import get_db_connection
+from ..postgresql.connector import get_db_connection
 
 def get_dated_photos():
     """retrieves photos with dates from database"""
-    conn = get_db_connection()
+    engine = get_db_connection('trainer')
     query = """
     SELECT date_taken, date_taken_granularity, owner_nsid, id
     FROM photo 
     WHERE date_taken IS NOT NULL
     """
-    df = pd.read_sql_query(query, conn)
-    conn.close()
-    print(f"Loaded {len(df)} photos)
+    df = pd.read_sql_query(query, engine)
+    print(f"Loaded {len(df):,} photos")
     return df
 
 def filter_dated_photos(df):
@@ -23,6 +22,9 @@ def filter_dated_photos(df):
     df = df.dropna(subset=['year'])
     df = df[(df['year'] >= 1850) & (df['year'] <= 2026)]
     df = df[df['owner_nsid'] != '12403504@N02'] # filtering the british library because all their dates are 2013 (unreliable dates)
-    print(f"Kept {len(df)} photos spanning {int(df['year'].min())}–{int(df['year'].max())}")
+    print(f"Kept   {len(df):,} photos spanning {int(df['year'].min())}–{int(df['year'].max())}")
     return df
+
+if __name__ == "__main__":
+    filter_dated_photos(get_dated_photos())
 
