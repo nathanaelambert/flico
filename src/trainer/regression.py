5079a800c986e6eacada43fa5226d014ff3677de
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-03-24 10:47:59 +0100

    SVR 20 years

--- src/trainer/regression.py
+++ src/trainer/regression.py
@@ -1,7 +1,13 @@
+import joblib
 import pandas as pd
 import numpy as np
 import ast
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.svm import SVR
+from sklearn.linear_model import Ridge
+from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from datetime import datetime
 from ..postgresql.connector import get_db_connection
@@ -21,12 +27,11 @@ def get_data():
     train = photos[photos["is_test_set"] == False]
     test = photos[photos["is_test_set"] == True]
 
-    return train.reset_index(drop=True), test.reset_index(drop=True)
+    return photos, train.reset_index(drop=True), test.reset_index(drop=True)
 
-def date_taken_regression(train):
+def date_taken_train_model(train, model):
     X = np.stack(train["sig_lip_vect_n"].values)
     y = train["year"]
-    model = LinearRegression()
     model.fit(X, y)
     y_pred = model.predict(X)
     mae = mean_absolute_error(y, y_pred)
@@ -50,14 +55,73 @@ def evaluate(model, test):
     print(f"Test   MeanAbsoluteError: {round(mae, 2)} years")
     return test, {"mae": mae}
 
-if __name__ == "__main__":
-    train, test = get_data()
-    model, train_error = date_taken_regression(train)
-    mae_random = random_baseline(train)
-    test_with_preds, metrics = evaluate(model, test)
+def save_predictions(model, photos):
+    pass
 
+
+if __name__ == "__main__":
+    photos, train, test = get_data()
+
+    # print("Random Benchmark:")
+    # mae_random = random_baseline(train)
+    """baseline random
+    Random MeanAbsoluteError: 57.59 years
+    """
+
+    # print("Linear Regression:")
+    # model_lin, mae_lin = date_taken_train_model(train, LinearRegression())
+    # test_preds_lin, metrics_lin = evaluate(model_lin, test)
     """ RESULTS (march 20 2026):
     Train  MeanAbsoluteError: 24.98 years
-    Random MeanAbsoluteError: 57.59 years
     Test   MeanAbsoluteError: 25.45 years
     """
+
+    # print("Ridge Regression:")
+    # ridge = Ridge(alpha=10.0)
+    # ridge, mae_ridge = date_taken_train_model(train, ridge)
+    # test_preds_ridge, metrics_ridge = evaluate(ridge, test)
+    """
+    alpha| test mae | train mae
+    10.0 | 25.40    | 24.92
+    02.0 | 25.42    | 24.92
+    01.0 | 25.41    | 24.92
+    """
+    
+    # print("degree 2 Polynomial Regression:")
+    # poly_model = make_pipeline(PolynomialFeatures(degree=2), LinearRegression())
+    # poly_model, mae_poly = date_taken_train_model(train, poly_model)
+    # test_preds_poly, metrics_poly = evaluate(poly_model, test)
+    """
+    ArrayMemoryError: Unable to allocate 70.0 GiB for an array with shape (63492, 296065) and data type float32
+    """
+
+    print("SupportVectorRegression:")
+    svr = SVR(kernel="rbf", C=50.0, gamma="scale")
+    svr, mae_svr = date_taken_train_model(train, svr)
+    joblib.dump(svr, "svr50_siglip320_model.joblib")
+    test_preds_svr, metrics_svr = evaluate(svr, test)
+    """
+        SupportVectorRegression: C=10
+    Train  MeanAbsoluteError: 19.61 years
+    Test   MeanAbsoluteError: 21.27 years
+
+        SupportVectorRegression: C=50
+    Train  MeanAbsoluteError: 15.14 years
+    Test   MeanAbsoluteError: 19.95 years
+
+        SupportVectorRegression: C=100
+    Train  MeanAbsoluteError: 12.37 years
+    Test   MeanAbsoluteError: 19.6 years
+    """
+
+    # print("Random Forest:")
+    # rf = RandomForestRegressor(n_estimators=300, max_depth=None, random_state=0)
+    # rf, mae_rf = date_taken_train_model(train, rf)
+    # test_preds_rf, metrics_rf = evaluate(rf, test)
+    
+    # print("Gradient Boosting:")
+    # gbr = GradientBoostingRegressor(random_state=0)
+    # gbr, mae_gbr = date_taken_train_model(train, gbr)
+    # test_preds_gbr, metrics_gbr = evaluate(gbr, test)
+
+    # Nearest Neighbor
