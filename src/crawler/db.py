231ab43d96dd834a0c29dc3c11c307d0a27704bd
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-03-26 21:27:22 +0100

    row count

--- src/crawler/db.py
+++ src/crawler/db.py
@@ -34,7 +34,7 @@ def insert_license(license):
 
 def insert_picture(p):
     with get_engine('crawler').connect() as conn:
-        conn.execute(text("""--sql
+        result =  conn.execute(text("""--sql
             INSERT INTO photo ( id,  owner_nsid,  secret,  server,  farm,  title,  is_public,  is_friend,  is_family,  license_id,  description,  original_width,  original_height,  date_upload,  last_update,  date_taken,  date_taken_granularity,  date_taken_unknown,  owner_name,   views,  tags,  machine_tags,  original_secret,  original_format,  latitude,  longitude,  accuracy,  context,  media,  media_status,  path_alias,  url_sq,  height_sq,  width_sq,  url_t,  height_t,  width_t,  url_s,  height_s,  width_s,  url_q,  height_q,  width_q,  url_m,  height_m,  width_m,  url_n,  height_n,  width_n,  url_z,  height_z,  width_z,  url_c,  height_c,  width_c,  url_l,  height_l,  width_l,  url_o,  height_o,  width_o)
             VALUES            (:id, :owner_nsid, :secret, :server, :farm, :title, :is_public, :is_friend, :is_family, :license_id, :description, :original_width, :original_height, :date_upload, :last_update, :date_taken, :date_taken_granularity, :date_taken_unknown, :owner_name,  :views, :tags, :machine_tags, :original_secret, :original_format, :latitude, :longitude, :accuracy, :context, :media, :media_status, :path_alias, :url_sq, :height_sq, :width_sq, :url_t, :height_t, :width_t, :url_s, :height_s, :width_s, :url_q, :height_q, :width_q, :url_m, :height_m, :width_m, :url_n, :height_n, :width_n, :url_z, :height_z, :width_z, :url_c, :height_c, :width_c, :url_l, :height_l, :width_l, :url_o, :height_o, :width_o)
             ON CONFLICT (id, owner_nsid) DO NOTHING
@@ -63,4 +63,6 @@ def insert_picture(p):
             "url_l": p.get("url_l"), "height_l": p.get("height_l"), "width_l": p.get("width_l"),
             "url_o": p.get("url_o"), "height_o": p.get("height_o"), "width_o": p.get("width_o"),
         }])
+        rowcount = result.rowcount
         conn.commit()
+    return rowcount > 0
