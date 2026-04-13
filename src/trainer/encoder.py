9c3c42158983718819bae7de9675a4792292499c
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-04-13 13:41:30 +0200

    untested encoder reformat

--- src/trainer/encoder.py
+++ src/trainer/encoder.py
@@ -3,10 +3,10 @@ import torch
 import requests
 from PIL import Image
 from transformers import AutoProcessor, AutoModel
-from sqlalchemy import text
 import numpy as np
 
-from src.db.connector import get_engine
+
+from src.trainer.db import photos_without_embedding
 
 model_name = "google/siglip-base-patch16-224"
 processor = AutoProcessor.from_pretrained(model_name)
@@ -23,36 +23,21 @@ def encode_with_siglip(url_n, size=320, vector_dimension=768):
     assert embedding.shape == (768,), f"Expected (768,), got {embedding.shape}"
     return embedding.astype(np.float32)
 
-def generate_sig_lip_embeddings():
+def generate_embeddings(embedding_name: str):
     """Generate sig_lip_vect_n embeddings in batch for machine_learning_photo table"""
-    engine = get_engine('trainer')
-    get_query = """
-    SELECT mlp.owner_nsid, mlp.id, p.url_n
-    FROM machine_learning_photo mlp
-    JOIN photo p ON (p.owner_nsid, p.id) = (mlp.owner_nsid, mlp.id)
-    WHERE mlp.sig_lip_vect_n IS NULL
-    """
-    photos = pd.read_sql_query(get_query, engine)
+    photos = photos_without_embdedding("siglip320")
     print(f"Found {len(photos)} photos")
-    for idx, row in photos.iterrows():
-        try:
-            embedding = encode_with_siglip(row['url_n'])
-            update_query = text("""
-            UPDATE machine_learning_photo 
-            SET sig_lip_vect_n = :embedding
-            WHERE owner_nsid = :owner_nsid AND id = :id
-        """)
-            with engine.connect() as conn:
-                conn.execute(update_query, {
-                    'embedding': embedding.tolist(),
-                    'owner_nsid': row['owner_nsid'],
-                    'id': row['id']
-                })
-                conn.commit()
-            print(f"{idx} : url: {row['url_n']} was embedded")
-        except Exception as e:
-            print(f"❌ owner_nsid = '{row['owner_nsid']}' AND id = {row['id']}\n {e}")
-            continue
+    pics_embedded = 0
+    for idx, photo in photos.iterrows():
+            try:
+                embedding = encode_with_siglip(photo['url_n']).tolist()
+                update_picture(id, owner_nsid, embedding_name, embedding)
+                pics_saved += 1
+            except Exception as e:
+                print(f"{c.RED}Embedding {photo['id']}: {e}{c.RESET}")
+
+
+       
 
 if __name__ == "__main__":
-    generate_sig_lip_embeddings()
+    generate_sig_lip_embeddings("sig_lip_vect_n")
