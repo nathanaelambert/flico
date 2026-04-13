cc7d39663a9ac8f2b7b125775718b40163b44491
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-04-13 13:39:28 +0200

    benchmark photos pre 2000

--- src/server/db.py
+++ src/server/db.py
@@ -16,3 +16,19 @@ def photos():
     df['page_url'] = df.apply(lambda row: f"https://www.flickr.com/photos/{row['owner_nsid']}/{row['id']}", axis=1)
     df['true_date'] = to_datetime(df['date_taken'], errors='coerce').dt.year.values
     return df
+
+def benchmark_photos():
+    query = text("""--sql
+            SELECT mlp.owner_nsid, mlp.id, mlp.is_test_set, 
+                    mlp.reg_n_pred_date, mlp.qwen3_pred_date,
+                    p.url_n, p.date_taken, p.date_taken_granularity,
+                    p.title, p.description, p.owner_name
+            FROM machine_learning_photo mlp
+            JOIN photo p ON (p.owner_nsid, p.id) = (mlp.owner_nsid, mlp.id)
+            WHERE mlp.reg_n_pred_date IS NOT NULL AND mlp.qwen3_pred_date IS NOT NULL
+        """)
+    df = read_sql_query(query, get_engine("server"))
+    df['page_url'] = df.apply(lambda row: f"https://www.flickr.com/photos/{row['owner_nsid']}/{row['id']}", axis=1)
+    df['true_date'] = to_datetime(df['date_taken'], errors='coerce').dt.year.values
+    df = df[df['true_date'] <= 1999]
+    return df
