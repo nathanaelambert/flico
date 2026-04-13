0ae294fb54d044522199f2ba4e6ed664d99a00e0
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-04-13 16:50:33 +0200

    db name safe

--- src/core/db.py
+++ src/core/db.py
@@ -15,14 +15,17 @@ def get_engine(user: Literal["trainer", "crawler", "server", "dev"]):
     load_dotenv()
     password_var = f"PWD{user.upper()}"
     password = os.getenv(password_var)
+    db = os.getenv('PGDATABASE')
 
     if not password:
         raise ValueError(f"Password not found for env var: {password_var}")
+    if not db:
+        raise ValueError(f"Database name not found. Check env var: PGDATABSE")
     
     connection_string = (
         f"postgresql+psycopg2://{user}:{password}"
         f"@{os.getenv('PGHOST')}:{os.getenv('PGPORT', '5432')}"
-        f"/{os.getenv('PGDATABASE')}"
+        f"/{db}"
     )
     engine = create_engine(connection_string, echo=False)
     
