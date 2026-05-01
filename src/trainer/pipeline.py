f6169df0758f286fa1f6cc14219cd703f76ee152
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-05-01 13:02:32 +0200

    pd no pandera

--- src/trainer/pipeline.py
+++ src/trainer/pipeline.py
@@ -1,29 +1,28 @@
 import pandas as pd
-import pandera.pandas as pa
-from pandera.typing import DataFrame
 from typing import List
 import src.trainer.db as db
 import src.server.db as server_db
-import src.core.schema as schema
 import src.trainer.date as date
 import src.trainer.geo as geo
 
 
 
-def learn_to_date(flickr_photos: DataFrame[schema.Photo]):
+def learn_to_date(flickr_photos: pd.DataFrame):
     # DATE TRAINING PIPELINE
     #   initial filtering and sampling
     valid_dates = date.processing.filter(flickr_photos)
     db.use_for_date(valid_dates)
     #   embeddings
-    # date_embeddings = date.embedding.siglip(src.core.db.view(flickr_photos, valid_dates))
-    # db.update_column('sig_lip_vect_n')(date_embeddings)
+    date_embeddings = date.embedding.siglip(db.flickr_mlphoto_to_embed())
+    db.update_siglip(date_embeddings)
     #   regression
     # TODO
     # store model somewhere
     #   prediction
 
-def learn_to_locate(flickr_photos: DataFrame[schema.Photo]):
+    
+
+def learn_to_locate(flickr_photos: pd.DataFrame):
     # GEO PIPELINE
     #   initial filtering and sampling
     valid_geo = geo.processing.filter(flickr_photos)
@@ -32,19 +31,20 @@ def learn_to_locate(flickr_photos: DataFrame[schema.Photo]):
     #   clustering
 
 
-def predict_date(photo: DataFrame[schema.Photo]) -> List[int]:
+def predict_date(photo: pd.DataFrame) -> List[int]:
     pass
 
-def predict_geo(photo: DataFrame[schema.Photo]) -> List[tuple[float, float]]:
+def predict_geo(photo: pd.DataFrame) -> List[tuple[float, float]]:
     pass
 
 if __name__ == "__main__":    
     # slow (loads millions of pics)
-    flickr_photos = db.flickr_photo()
-    learn_to_date(flickr_photos)
-
     db.rm_data_ml_photo('is_date_train')
     db.rm_data_ml_photo('is_date_test')
+    
+    # flickr_photos = db.flickr_photo()
+    # learn_to_date(flickr_photos)
+
     # """
     # start the app
     # - shows status
