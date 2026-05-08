02c9e559fef41fdea15e702a2c204d393334b068
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-05-08 14:35:01 +0200

    geo processing doc

--- src/trainer/geo/processing.py
+++ src/trainer/geo/processing.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 def filter(photos: pd.DataFrame) -> pd.DataFrame:
-    """Only keep rows for the pictures wanted for geo analysis"""
+    """
+    Only keep rows for the pictures wanted for geo analysis
+    input df: all columns from photo
+    output df: owner_nsid and id of the kept photos
+    """
     photos = photos[photos['accuracy'] != 0]
     return photos[['owner_nsid', 'id']]
