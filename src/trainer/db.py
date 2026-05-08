088a2928c55eafc01f4e3c8ca5668c3e8176b3ce
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-05-08 16:00:52 +0200

    rm limit 10

--- src/trainer/db.py
+++ src/trainer/db.py
@@ -13,7 +13,7 @@ from src.core.decorator import memoize
 def flickr_photo() -> pd.DataFrame:
     query = text("""--sql
         SELECT * FROM photo
-        LIMIT 10 --TODO remove this line later
+        --LIMIT 10 --TODO remove this line later
     """)
     return pd.read_sql_query(query, get_engine("trainer"))
 
