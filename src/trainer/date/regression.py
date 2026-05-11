4866efaa374f6742c90c9fbeee594c09c9a86523
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-05-11 16:22:14 +0200

    using hugging face model uploaded

--- src/trainer/date/regression.py
+++ src/trainer/date/regression.py
@@ -1,6 +1,6 @@
-import joblib
 import pandas as pd
 import numpy as np
+import joblib
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.svm import SVR
@@ -9,11 +9,35 @@ from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from datetime import datetime
-from sqlalchemy import text
-from src.trainer.db import photos_with_siglip_enbedding
+from huggingface_hub import hf_hub_download
+import src.utils.colors as c
 
+print(f"Loading HuggingFace model for SVR50 regression...{c.GREY}")
+model_path = hf_hub_download(
+    repo_id="nathanaelambert/svr50siglip320flico-05-2026",
+    filename="svr50_siglip320_model.joblib"
+)
+svr_model = joblib.load(model_path)
+print(f"Model loaded successfully!{c.GREY}")
 
-def date_taken_train_model(train, model):
+def svr50_predictions(df: pd.DataFrame)-> pd.DataFrame:
+    """ predicts dates from siglip embedding using SVR trained model"""
+    X = np.stack(df['sig_lip_vect_n'].values)
+    preds = svr_model.predict(X)
+    df = df.copy()
+    df['reg_n_pred_date'] = preds
+    print(f"Predicted dates for {len(df)} pictures.")
+    return df
+
+def train_model(train: pd.DataFrame, test: pd.DataFrame):
+    ## NOT TESTED
+    """trains and evaluate a regression model given the provided data"""
+    svr = SVR(kernel="rbf", C=50.0, gamma="scale")
+    svr, mae_svr = _date_taken_train_model(train, svr)
+    joblib.dump(svr, "models/svr50_siglip320_model.joblib")
+    test_preds_svr, metrics_svr = _evaluate(svr, test)
+
+def _date_taken_train_model(train, model):
     """"trains and returns the given models on the given data"""
     X = np.stack(train["sig_lip_vect_n"].values)
     y = train["year"]
@@ -23,15 +47,15 @@ def date_taken_train_model(train, model):
     print(f"Train  MeanAbsoluteError: {round(mae, 2)} years")
     return model, mae
 
-def random_baseline(train):
+def _random_baseline(train):
     """evaluate the performance of a random output"""
     np.random.seed(42)
     random_preds = np.random.randint(1850, 2027, size=len(train))
     mae_random = mean_absolute_error(train["year"], random_preds)
-    print(f"Random MeanAbsoluteError: {round(mae_random,2)} years")  # Should be ~44
+    print(f"Random MeanAbsoluteError: {round(mae_random,2)} years")
     return mae_random
 
-def evaluate(model, test):
+def _evaluate(model, test):
     """turns embeddings into a prediction using the model"""
     X_test = np.stack(test["sig_lip_vect_n"].values)
     y_test = test["year"]
@@ -41,101 +65,62 @@ def evaluate(model, test):
     mae = mean_absolute_error(y_test, y_pred)
     print(f"Test   MeanAbsoluteError: {round(mae, 2)} years")
     return test, {"mae": mae}
+   
+"""
+    SupportVectorRegression: C=10
+Train  MeanAbsoluteError: 19.61 years
+Test   MeanAbsoluteError: 21.27 years
 
-def save_predictions(model, photos):
-    """writes predicted dates to the databse"""
-    engine = get_db_connection("trainer")
-    for idx, row in photos.iterrows():
-        try:
-            prediction = row['svr_50']
-            update_query = text("""
-            UPDATE machine_learning_photo 
-            SET reg_n_pred_date = :prediction
-            WHERE owner_nsid = :owner_nsid AND id = :id
-        """)
-            with engine.connect() as conn:
-                conn.execute(update_query, {
-                    'prediction': int(prediction),
-                    'owner_nsid': row['owner_nsid'],
-                    'id': row['id']
-                })
-                conn.commit()
-            print(f"{idx} : was dated from {prediction}")
-        except Exception as e:
-            print(f"❌ owner_nsid = '{row['owner_nsid']}' AND id = {row['id']}\n {e}")
-            continue
+    SupportVectorRegression: C=50
+Train  MeanAbsoluteError: 15.14 years
+Test   MeanAbsoluteError: 19.95 years
 
+    SupportVectorRegression: C=100
+Train  MeanAbsoluteError: 12.37 years
+Test   MeanAbsoluteError: 19.6 years
+"""
 
-if __name__ == "__main__":
-    photos, train, test = photos_with_siglip_enbedding()
+# print("Random Benchmark:")
+# mae_random = _random_baseline(train)
+"""baseline random
+Random MeanAbsoluteError: 57.59 years
+"""
 
-    """SVR has been best performing in a reasonable time (see alternatives below)"""
-    print("SupportVectorRegression:")
-    svr = joblib.load("models/svr50_siglip320_model.joblib")
-    if svr:
-        preds_svr, metrics_svr = evaluate(svr, photos)
-        photos['svr_50'] = preds_svr["prediction"].values
-        save_predictions(svr, photos)
-    else:
-        svr = SVR(kernel="rbf", C=50.0, gamma="scale")
-        svr, mae_svr = date_taken_train_model(train, svr)
-        joblib.dump(svr, "models/svr50_siglip320_model.joblib")
-        test_preds_svr, metrics_svr = evaluate(svr, test)
-    """
-        SupportVectorRegression: C=10
-    Train  MeanAbsoluteError: 19.61 years
-    Test   MeanAbsoluteError: 21.27 years
+# print("Linear Regression:")
+# model_lin, mae_lin = _date_taken_train_model(train, LinearRegression())
+# test_preds_lin, metrics_lin = _evaluate(model_lin, test)
+""" RESULTS (march 20 2026):
+Train  MeanAbsoluteError: 24.98 years
+Test   MeanAbsoluteError: 25.45 years
+"""
 
-        SupportVectorRegression: C=50
-    Train  MeanAbsoluteError: 15.14 years
-    Test   MeanAbsoluteError: 19.95 years
+# print("Ridge Regression:")
+# ridge = Ridge(alpha=10.0)
+# ridge, mae_ridge = _date_taken_train_model(train, ridge)
+# test_preds_ridge, metrics_ridge = _evaluate(ridge, test)
+"""
+alpha| test mae | train mae
+10.0 | 25.40    | 24.92
+02.0 | 25.42    | 24.92
+01.0 | 25.41    | 24.92
+"""
 
-        SupportVectorRegression: C=100
-    Train  MeanAbsoluteError: 12.37 years
-    Test   MeanAbsoluteError: 19.6 years
-    """
+# print("degree 2 Polynomial Regression:")
+# poly_model = make_pipeline(PolynomialFeatures(degree=2), LinearRegression())
+# poly_model, mae_poly = _date_taken_train_model(train, poly_model)
+# test_preds_poly, metrics_poly = _evaluate(poly_model, test)
+"""
+ArrayMemoryError: Unable to allocate 70.0 GiB for an array with shape (63492, 296065) and data type float32
+"""
 
-    # print("Random Benchmark:")
-    # mae_random = random_baseline(train)
-    """baseline random
-    Random MeanAbsoluteError: 57.59 years
-    """
+# print("Random Forest:")
+# rf = RandomForestRegressor(n_estimators=300, max_depth=None, random_state=0)
+# rf, mae_rf = _date_taken_train_model(train, rf)
+# test_preds_rf, metrics_rf = _evaluate(rf, test)
 
-    # print("Linear Regression:")
-    # model_lin, mae_lin = date_taken_train_model(train, LinearRegression())
-    # test_preds_lin, metrics_lin = evaluate(model_lin, test)
-    """ RESULTS (march 20 2026):
-    Train  MeanAbsoluteError: 24.98 years
-    Test   MeanAbsoluteError: 25.45 years
-    """
+# print("Gradient Boosting:")
+# gbr = GradientBoostingRegressor(random_state=0)
+# gbr, mae_gbr = _date_taken_train_model(train, gbr)
+# test_preds_gbr, metrics_gbr = _evaluate(gbr, test)
 
-    # print("Ridge Regression:")
-    # ridge = Ridge(alpha=10.0)
-    # ridge, mae_ridge = date_taken_train_model(train, ridge)
-    # test_preds_ridge, metrics_ridge = evaluate(ridge, test)
-    """
-    alpha| test mae | train mae
-    10.0 | 25.40    | 24.92
-    02.0 | 25.42    | 24.92
-    01.0 | 25.41    | 24.92
-    """
-    
-    # print("degree 2 Polynomial Regression:")
-    # poly_model = make_pipeline(PolynomialFeatures(degree=2), LinearRegression())
-    # poly_model, mae_poly = date_taken_train_model(train, poly_model)
-    # test_preds_poly, metrics_poly = evaluate(poly_model, test)
-    """
-    ArrayMemoryError: Unable to allocate 70.0 GiB for an array with shape (63492, 296065) and data type float32
-    """
-
-    # print("Random Forest:")
-    # rf = RandomForestRegressor(n_estimators=300, max_depth=None, random_state=0)
-    # rf, mae_rf = date_taken_train_model(train, rf)
-    # test_preds_rf, metrics_rf = evaluate(rf, test)
-    
-    # print("Gradient Boosting:")
-    # gbr = GradientBoostingRegressor(random_state=0)
-    # gbr, mae_gbr = date_taken_train_model(train, gbr)
-    # test_preds_gbr, metrics_gbr = evaluate(gbr, test)
-
-    # Nearest Neighbor
+# Nearest Neighbor
