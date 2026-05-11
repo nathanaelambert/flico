7abcdd26638a7429d49f3b998e5bda40f6cb8d81
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-05-11 16:21:57 +0200

    rename file

--- src/trainer/date/description.py
+++ src/trainer/date/description.py
@@ -1,8 +1,11 @@
-from src.trainer.db import photos_with_description
-
+import pandas as pd
 def predict_date(description: str) -> int:
     date = 1850
     if description == 'taken in 2020':
         date = 2020
     return date
 
+def explore(df: pd.DataFrame):
+
+
+    pass
