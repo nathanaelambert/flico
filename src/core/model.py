8745f9ead125432ccc81ed84f0033894706f162c
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-05-19 18:54:03 +0200

    geo cluster model

--- src/core/model.py
+++ src/core/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import (Table, Column, BigInteger, Text, Integer, Boolean,
+from sqlalchemy import (Table, Column, BigInteger, Text, Integer, Boolean, Float,
                         ForeignKeyConstraint, MetaData)
 from pgvector.sqlalchemy import VECTOR
 
@@ -11,19 +11,19 @@ ml_photo_table = Table(
     Column("owner_nsid", Text, primary_key=True),
     Column("id", BigInteger, primary_key=True),
     Column("geo_cluster_id", Integer),
-    
+    Column("is_building", Boolean),
+    Column("p_building", Float),
+
     Column("is_test_set", Boolean),  # deprecated ?
 
     Column("is_date_test", Boolean),
     Column("is_date_train", Boolean),
 
-    Column("is_geo_test", Boolean),
-    Column("is_geo_train", Boolean),
-
     Column("sig_lip_vect_n", VECTOR(768)),
     Column("sig_lip_vect_o", VECTOR(768)),
-    Column("reg_n_pred_date", Integer),
+    Column("clip_vect_224", VECTOR(512)),
 
+    Column("reg_n_pred_date", Integer),
     Column("qwen3_pred_date", Integer),
     Column("descr_pred_date", Integer),
 
@@ -31,8 +31,8 @@ ml_photo_table = Table(
         ["owner_nsid", "id"],
         ["photo.owner_nsid", "photo.id"]
     ),
-    ForeignKeyConstraint(
-        ["geo_cluster_id"],
-        ["geo_cluster.id"]
-    ),
+    # ForeignKeyConstraint(
+    #     ["geo_cluster_id"],
+    #     ["geo_cluster.id"]
+    # ),
 )
